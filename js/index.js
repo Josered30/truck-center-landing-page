455c3451380af2dab966d2e592983e8d14ec5f68
Jose Aparcana <josered30@hotmail.com>
2021-11-07 15:00:45 -0500

    form

--- js/index.js
+++ js/index.js
@@ -1,25 +1,29 @@
-// document.addEventListener("DOMContentLoaded", function(event) { 
+// document.addEventListener("DOMContentLoaded", function(event) {
 // });
 
-
 const form = document.getElementById("emailForm");
-    
-async function handleSubmit(event) {
+
+function handleSubmit(event) {
   event.preventDefault();
   const status = document.getElementById("emailFormStatus");
   const data = new FormData(event.target);
+
   fetch(event.target.action, {
     method: form.method,
     body: data,
     headers: {
-        'Accept': 'application/json'
-    }
-  }).then(response => {
-    status.innerHTML = "Datos enviados";
-    form.reset()
-  }).catch(error => {
-    status.innerHTML = "Ocurrio un error en el envio"
-  });
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "Datos enviados";
+      status.classList.toggle("status-succeed");
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = "Ocurrio un error en el envio";
+      status.classList.toggle("status-fail");
+    });
 }
 
-form.addEventListener("submit", handleSubmit)
+form.addEventListener("submit", handleSubmit);
